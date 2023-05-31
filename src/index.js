b5f8fde1ddef5bebbab86f90e12f8f15638997d2
Gustavo Santos <z.gsantos20@gmail.com>
2023-05-31 00:47:33 +0000

    Change the product logic for respective folders

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-const http = require('http')
-const users = require('./mocks/index')
+const http = require("http");
+const { URL } = require("url");
+
+const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-  if(request.url === '/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json'});
-    response.end(JSON.stringify(users))
-  } else {
-    response.writeHead(404, { 'Content-Type': 'text/html'});
-    response.end(`Cannot ${request.method} ${request.url}`)
-  }
-})
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
+  const parsedParams = Object.fromEntries(parsedUrl.searchParams)
 
-server.listen(3000, () => console.log('Server started at http://localhost:3000'))
+  const route = routes.find(
+    (routObject) =>
+      routObject.endpoint === parsedUrl.pathname &&
+      routObject.method === request.method
+  );
+
+  if (route) {
+    route.handler({ ...request, query: parsedParams }, response);
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+  }
+});
+
+server.listen(3000, () =>
+  console.log("Server started at http://localhost:3000")
+);
