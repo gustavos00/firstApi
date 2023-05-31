6d9842273ce5830f3c02140679c3f893ae42f430
Gustavo Santos <z.gsantos20@gmail.com>
2023-05-31 00:22:47 +0000

    First commit - Start the http server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const users = require('./mocks/index')
+
+const server = http.createServer((request, response) => {
+  if(request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json'});
+    response.end(JSON.stringify(users))
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html'});
+    response.end(`Cannot ${request.method} ${request.url}`)
+  }
+})
+
+server.listen(3000, () => console.log('Server started at http://localhost:3000'))
---
+++ src/mocks/index.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { id: 1, name: "André" },
+  { id: 2, name: "João" },
+  { id: 3, name: "Mónica" },
+];
